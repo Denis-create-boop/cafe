2a8cc9776f0cfedb7e04fbf275cddbb9eab43f09
denis <Den_1994_2015@mail.ru>
2025-01-16 09:20:10 +0300

    сделал переход по страницам в пагинации

--- my_cafe/dishes/views.py
+++ my_cafe/dishes/views.py
@@ -4,16 +4,17 @@ from django.core.paginator import Paginator
 from dishes.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     dishes = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(dishes, 3)
-    current_page = paginator.page(1)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Little Italy - Каталог",
         "dishes": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "dishes/catalog.html", context)
 
