d6e9a5108d47cbccf6800ecec215a45cb47fe3da
denis <Den_1994_2015@mail.ru>
2025-01-17 13:44:43 +0300

    added search

--- my_cafe/dishes/views.py
+++ my_cafe/dishes/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render, get_list_or_404
 from django.core.paginator import Paginator
 
 from dishes.models import Products
+from dishes.utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     page = request.GET.get("page", 1)
     on_sale = request.GET.get("on_sale", None)
     order_by = request.GET.get("order_by", None)
+    query = request.GET.get('q', None)
 
-    dishes = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    if query:
+        dishes = q_search(query)
+    else:
+        dishes = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     if on_sale:
+        dishes = Products.objects.filter(category__slug=category_slug)
         dishes = dishes.filter(discount__gt=0)
     if order_by and order_by != "default":
+        dishes = Products.objects.filter(category__slug=category_slug)
         dishes = dishes.order_by(order_by)
 
     paginator = Paginator(dishes, 3)
