5ed9a825a32c638bae2960e99ef302b5751e3c57
denis <Den_1994_2015@mail.ru>
2025-01-17 20:05:12 +0300

    acreated database LittleItaly in postgresql

--- my_cafe/my_cafe/settings.py
+++ my_cafe/my_cafe/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     
     'main',
     'dishes',
@@ -78,9 +79,13 @@ WSGI_APPLICATION = 'my_cafe.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "LittleItaly",
+        "USER": "LittleItally",
+        "PASSWORD": "littleitaly",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
